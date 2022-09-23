d922020218cc345e937869f3b246300f2018134d
Ahsan Omerjee <84373001+ahsan2002@users.noreply.github.com>
2022-09-24 01:42:40 +0500

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,27 +1,3 @@
-// import React, { useState } from "react";
-
-
-// const App=()=>{
-//     const state=useState();
-//     // console.log(state);
-//     const [count,setCount]=useState(0);
-
-//     // let count=1;
-    
-//     const Incnum=()=>{
-//         setCount(count + 1);
-//         // console.log("clicked "+ count++);
-//     };
-//     return(
-//         <>
-//         <h1>{count}</h1>
-//         <button onClick={Incnum}>Click me</button>
-//         </>
-//     )
-// };
-
-// export default App;
-
 import React, { useState } from "react";
 import "./index.css";
 
@@ -41,8 +17,6 @@ const App=()=>{
         <div>
         <h1>{ctime}</h1>
         </div>
-        {/* through button */}
-        {/* <button onClick={Updatetime}>Get Current Time</button> */}
         </>
     );
 }
